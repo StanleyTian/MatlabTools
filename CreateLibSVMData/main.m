3b735c3ad5bea8deb2d833cce4b51c8c16717712
史丹利复合田 <b20130525@xs.ustb.edu.cn>
2016-12-13 10:43:47 +0800

    移除无用的图像数量指示，由程序自动判断。

--- CreateLibSVMData/main.m
+++ CreateLibSVMData/main.m
@@ -1,10 +1,9 @@
 clear;
 clc;
 folder_path = '/Users/shidanlifuhetian/Desktop/TestField/sampleB/';
-total_images_count = 58;
 class = '-1';
 output_file_full_path = '/Users/shidanlifuhetian/Desktop/TestField/Bdata_scale.txt';
 
-lbps = images_to_lbps(folder_path,total_images_count);
+lbps = images_to_lbps(folder_path);
 libsvm_data = lbps_to_libsvm_data(lbps,class);
 libsvm_data_to_file(libsvm_data,output_file_full_path);
